e196101f339a48b986b5dde05a0de5778a9daa55
asokolov@groupstp.ru <Wrin@,queswij3>
2019-06-27 13:59:55 +0800

    searching user by email except loginLowerCase

--- modules/core/src/com/groupstp/googleoauth/service/SocialRegistrationServiceBean.java
+++ modules/core/src/com/groupstp/googleoauth/service/SocialRegistrationServiceBean.java
@@ -37,7 +37,7 @@ public class SocialRegistrationServiceBean implements SocialRegistrationService 
 
         // Find existing user
         TypedQuery<User> query = em.createQuery(
-                "select u from sec$User u where u.loginLowerCase = :email and (u.active = true or u.active is null)",
+                "select u from sec$User u where u.email = :email and (u.active = true or u.active is null)",
                 User.class);
         query.setParameter("email", data.getEmail() == null ? StringUtils.EMPTY : data.getEmail().toLowerCase());
         query.setViewName(View.LOCAL);
